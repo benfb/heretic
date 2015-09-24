743722350f18c029edc86e1997c7152e0152bd83
Ben Bailey <bennettbailey@gmail.com>
2015-09-23 22:15:30 -0400

    finalize a-to-an

--- src/heretic/core.clj
+++ src/heretic/core.clj
@@ -1,12 +1,12 @@
 (ns heretic.core
   (:gen-class)
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as str]))
 
 (defn read-list
   "Reads in a list of strings from a file."
   [path]
   (let [list-text (slurp path)]
-    (s/split list-text #"\n")))
+    (str/split list-text #"\n")))
 
 (def decades ["00s" "60s" "70s" "80s" "90s"])
 (def bands (read-list "resources/bands.txt"))
@@ -57,21 +57,45 @@
         fs))
     fs))
 
-(defn a-to-an
+(defn a-to-an-second
   "Converts a to an when appropriate."
   [s]
   (let [a (.indexOf s " a ")
         next-word (+ a 3)]
     (if (and (not= a -1) (vowel? (.charAt s next-word)))
-      (str (subs s 0 a) " an " (a-to-an (subs s next-word)))
+      (str (subs s 0 a) " an " (a-to-an-second (subs s next-word)))
       s)))
+
+(defn find-a-list
+  "Finds ' a ' in a string."
+  [s]
+  (take-while (partial < -1) (iterate #(.indexOf s " a " (+ %1 1)) (.indexOf s " a "))))
+
+;;(defn a-to-an-new
+  ;;"Converts a to an when appropriate."
+  ;;[s]
+  ;;(let [a-list (find-a-list s)
+        ;;vowels (filter #(vowel? (.charAt s (+ %1 3))) a-list)]
+    ;;; filter out indices of ' a 's which need not be replaced
+    ;;(filter #(vowel? (.charAt s (+ %1 3))) a-list)))
+    ;;;(subs s 0 i0) " an " (subs s i0+4 i1) " an " (subs s i1+4 i2)
+
+(defn a-to-an
+  "Converts a to an when appropriate."
+  [s]
+  (str/replace s #"( a )([aeiouAEIOU])" " an $2"))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Original")
-  (dotimes [_ 10] (time (a-to-an-original (finalString))))
-  (println "New")
-  (dotimes [_ 10] (time (a-to-an (finalString))))
+  (dotimes [_ 10]
+    (let [fs (finalString)]
+      (println "Original")
+      (time (a-to-an-original fs))
+      (println "Second")
+      (time (a-to-an-second fs))
+      (println "New")
+      (time (a-to-an fs))))
   (println (a-to-an (finalString))))
 
